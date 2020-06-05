4b4ce7ef33b477de37ca884385d4852d345f8019
quangloc.nguyen <quangloc.nguyen>
2020-06-05 14:37:39 +0700

    router

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser')
-const morgan = require('morgan');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
+const router = require("./router")
+
+// App
+app.use(morgan("combined"));
+app.use(bodyParser.json({type:'*/*'}));
+router(app);
 
 
+// Server
 const port = process.env.PORT || 4000;
 const server = http.createServer(app);
 server.listen(port);
 
-console.log('Server listening on:', port);
+console.log("Server listening on:", port);
