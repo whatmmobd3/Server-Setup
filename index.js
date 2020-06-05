c286fc2add1825917c62c5ae9acb500e7922f68c
quangloc.nguyen <quangloc.nguyen>
2020-06-05 14:00:18 +0700

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser')
+const morgan = require('morgan');
+const app = express();
+
+
+const port = process.env.PORT || 4000;
+const server = http.createServer(app);
+server.listen(port);
+
+console.log('Server listening on:', port);
