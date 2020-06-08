72e1b207c65f6f30a2916b637d664e22581c88a1
quangloc.nguyen <quangloc.nguyen>
2020-06-08 14:18:47 +0700

    jwt

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,11 +1,26 @@
 const User = require("../models/user");
+const jwt = require("jwt-simple");
+
+const config = require("../config");
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode(
+    {
+      sub: user.id,
+      iat: timestamp,
+    },
+    config.secret
+  );
+}
 
 exports.signup = function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
   if (!email || !password) {
-      return res.status(422).send({error:'You must provide email and password'})
+    return res
+      .status(422)
+      .send({ error: "You must provide email and password" });
   }
 
   User.findOne({ email: email }, function (err, existing) {
@@ -26,7 +41,9 @@ exports.signup = function (req, res, next) {
       if (err) {
         return next(err);
       }
-      res.json({success: true});
+
+      // res.json({ success: true, data: user });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
