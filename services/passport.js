d610d975dc7ece3c6bc844afad2798473bab513f
quangloc.nguyen <quangloc.nguyen>
2020-06-09 10:26:45 +0700

    passport authorzation

--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,24 @@ const User = require("../models/user");
 const config = require("../config");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey:config.secret
+};
+
+const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
+  User.findById(payload.sub, function (err, user) {
+    if (err) {
+      return done(err, false);
+    }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+passport.use(jwtLogin)
